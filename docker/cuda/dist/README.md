618d2f9064e4636f96ac4294d584b80f26351f17
zhangdongkun98 <zhangdongkun98@gmail.com>
2021-12-20 19:48:16 +0800

    sth

--- docker/cuda/dist/README.md
+++
@@ -1,5 +0,0 @@
-# DO NOT EDIT
-
-**All files in this directory are auto-generated!**
-
-Please edit the templates instead and use manager.py to regenerate.
--- docker/cuda/test/scripts/bats_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/bats-core/bats-core.git
-cd bats-core
-./install.sh /usr/local
-
-mkdir -p /opt/bats/assert
-git clone https://github.com/jasonkarns/bats-assert-1.git /opt/bats/assert
